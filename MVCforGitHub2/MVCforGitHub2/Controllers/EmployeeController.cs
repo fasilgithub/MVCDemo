b26798d179a328766a3779ef8a438cd97a79df36
Mohammed Fasil Firoz B <mohammedfasilfiroz.b@dxc.com>
2020-09-08 15:20:52 +0530

    new

--- MVCforGitHub2/MVCforGitHub2/Controllers/EmployeeController.cs
+++ MVCforGitHub2/MVCforGitHub2/Controllers/EmployeeController.cs
@@ -13,5 +13,9 @@ namespace MVCforGitHub2.Controllers
         {
             return View();
         }
+        public ActionResult Home()
+        {
+            return View();
+        }
     }
 }
